0b7ee4d818d949191c4d94cca4dffe3ab5de4855
Reginaldo Motta <reginaldomotta@users.noreply.github.com>
2023-12-29 21:06:09 -0400

    tab folclore
    part 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ToastController} from '@ionic/angular'
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  constructor(public toastController: ToastController) {}
 
-  constructor() {}
+  openSite(url: string) {
+    window.open(url);
+  }
 
+  async like(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Obrigado por curtir',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 }
